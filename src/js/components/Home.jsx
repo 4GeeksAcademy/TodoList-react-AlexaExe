043f9098772de850d171153e1bc359a29c8b9ff6
Alex <alexa.exebio@gmail.com>
2025-12-18 23:15:41 +0000

    me falta acabarlo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,79 +6,94 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
- const [alumno, setAlumno] = useState({
-	name:"",
-	email:"",
+	const [alumno, setAlumno] = useState({
+		name: "",
+		email: "",
 
- });	
+	});
 
- //const [email, setEmail] = useState("");
+	//const [email, setEmail] = useState("");
 
 
- const [alumnos, setAlumnos] =useState([]);	
- 
+	const [alumnos, setAlumnos] = useState([]);
+	console.log(alumno);
 
-const handleChange = (e) => {
+
+
+	const handleChange = (e) => {
+		setAlumno({
+			...alumno,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const handleKeyUp = (e) => {
+		if (alumno.name.trim() == "" || alumno.email.trim() == "") {
+			alert("Debes rellenar todos los campos, no seas flojo")
+			return
+		}
+
+		//spread operator
+		setAlumnos([...alumnos, alumno])
+		setAlumno({
+			name: "",
+			email: "",
+		})
+	}
+}
+
+
+const handleClick = () => {
+	if (alumno.name.trim() == "" || alumno.email.trim() == "") {
+		alert("Debes rellenar todos los campos, no seas flojo")
+		return
+	}
+
+	//spread operator
+	setAlumnos([...alumnos, alumno])
 	setAlumno({
-		...alumno,
-		[e.target.name]: e.target.value
+		name: "",
+		email: "",
 	})
 }
 
 
- const handleClick = () => {
- 	if(name.trim() =="" || email.trim() == ""){
-		alert("Debes rellenar todos los campos, no seas flojo")
-		return
-	}
-	
-//spread operator
-	setAlumnos ([...alumnos, {name: name, email: email}])
-	setName("")
-	setEmail("")
-}
-
-//const handleInputChange =(e) =>{
-	//setInputValue({
-		//...alumno,
-		//[e.target.name]:e.target.value})
-
-	//}
-
-	return (
-		<>
+return (
+	<>
 		<div className="text-center">
-            <input name="name" 
-			value={alumno.name} 
-			type="text" 
-			onChange={handleChange}/>
+			<input name="name"
+				value={alumno.name}
+				type="text"
+				onChange={handleChange} />
 
 
-			<input name="email" 
-			value={alumno.email} 
-			type="text" 
-			onChange={handleChange}/>
+			<input name="email"
+				value={alumno.email}
+				type="text"
+				onChange={handleChange}
+				onKeyUp={handleKeyUp}
+			/>
 
 
 
 			<button className="btn success" onClick={handleClick}>Agregar</button>
 			<div className="container">
 				{
-					alumnos.map((alumno,index) => {
-						return(
+					alumnos.map((alumno, index) => {
+						return (
 							<div key={index}>
 								<p> Name: {alumno.name}</p>
 								<p> Email: {alumno.email}</p>
-							</div>	
+							</div>
 						)
 					})
 				}
 
 			</div>
 		</div>
-		
-		</>
-	);
+
+	</>
+);
 
 }
 
